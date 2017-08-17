71d431a9cfa90fb8b6631adf3e4e372b9d317754
Rasmus Eskola <fruitiex@gmail.com>
2017-08-17 15:47:26 +0300

    Lecture 4

--- src/Components.js
+++ src/Components.js
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 
 export const ExerciseContainer = styled.div`width: 600px;`;
 
+export const Flex = styled.div`display: flex;`;
+export const Small = styled.div`font-size: 24px;`;
+export const Tiny = styled.div`
+  font-size: 20px;
+  text-align: center;
+`;
+
 export const Handwriting = styled.div`
   font-family: 'Gloria Hallelujah', cursive;
   opacity: 0.5;
--- src/Intro.js
+++ src/Intro.js
@@ -9,6 +9,9 @@ import {
   NumberedList,
   List,
   Item,
+  Flex,
+  Small,
+  Tiny,
 } from './Components';
 
 import Step from './Step';
@@ -24,6 +27,10 @@ import Example1 from './assets/example-1.png';
 import Example2 from './assets/example-2.png';
 import Example3 from './assets/example-3.png';
 import NavigationExample from './assets/nested-android.png';
+import FlexExample from './assets/flex-example.png';
+import NetworkExample from './assets/network-example.png';
+import ListExample from './assets/example-list.png';
+import TextExample from './assets/text-example.png';
 
 import logo from './assets/logo-react-native.svg';
 import './App.css';
@@ -312,6 +319,7 @@ class Intro extends Component {
                   These in turn map to Android, iOS (and others) native
                   representations of the respective component
                 </li>
+                <li>All components need to be imported</li>
               </ul>
             </CardContent>
           </Card>
@@ -657,30 +665,409 @@ const AppNavigator = TabNavigator({
             </CardContent>
           </Card>
         </Step>
-        <Step id="handling-text-input" x={100} y={1100} z={200} rotate={-5}>
+        <Step id="lecture-2" x={1100} y={1100} z={200} rotate={-5}>
           <Card>
             <CardContent>
-              <h4>Handling text input</h4>
-              TODO
-              <ul>
-                <li>This is similar to React, we just didn't cover it yet</li>
-              </ul>
+              <center>
+                <div className="App-header">
+                  <div>Lecture 4: Advanced React Native</div>
+                </div>
+              </center>
+              <List>
+                <Item>Network requests</Item>
+                <Item>Displaying data in lists</Item>
+                <Item>Handling text input</Item>
+                <Item>Styling</Item>
+                <Item>Layout and flexbox</Item>
+                <Item>React Native libraries</Item>
+                <Item>Coding exercise 2</Item>
+              </List>
             </CardContent>
           </Card>
         </Step>
-        <Step id="flexbox-layout" x={100} y={1800} z={200} rotate={-5}>
+        <Step id="network-requests" x={1300} y={1800} z={200} rotate={-7}>
           <Card>
             <CardContent>
-              <h4>Layout and flexbox</h4>
-              TODO
-              <ul>
-                <li>By default, React Native uses flexbox layout</li>
-                <li>Works very similar to flexbox on the web</li>
-                <li>Flex direction is set to "column" by default</li>
-                <ul style={{ marginTop: 0, fontSize: 30 }}>
-                  <li>(as phones tend to have portrait displays)</li>
-                </ul>
-              </ul>
+              <h4>Network requests</h4>
+              <List>
+                <Item>React Native provides API:s familiar from the web:</Item>
+                <List>
+                  <Item>Fetch API</Item>
+                  <Item>
+                    XMLHttpRequest API (<b>XHR</b>)
+                  </Item>
+                  <Item>WebSocket support</Item>
+                </List>
+                <Item>
+                  These API:s work the same way as on the web, so any JS library
+                  using these should work fine
+                </Item>
+                <List>
+                  <Item>Axios</Item>
+                  <Item>SuperAgent</Item>
+                  <Item>request</Item>
+                  <Item>SockJS</Item>
+                  <Item>etc...</Item>
+                </List>
+              </List>
+            </CardContent>
+          </Card>
+        </Step>
+        <Step id="network-requests-2" x={1300} y={2500} z={200} rotate={-7}>
+          <Flex style={{ marginLeft: -100, alignItems: 'center' }}>
+            <div style={{ width: 800 }}>
+              <Card>
+                <CardContent>
+                  <h6>
+                    Let's make a simple network request using{' '}
+                    <code>fetch()</code>:
+                  </h6>
+                  <PrismCode component="pre" className="language-jsx">
+                    {`import { Text } from 'react-native';
+
+class GitHubAPI extends React.Component {
+  state = { data: {} };
+
+  componentDidMount() {
+    fetch('https://api.github.com/search/repositories?q=react')
+      .then((response) => response.json())
+      .then((data) => this.setState({ data }))
+  }
+
+  render = () =>
+    <Text>
+      { JSON.stringify(this.state.data, 0, 2) }
+    </Text>;
+}
+                  `}
+                  </PrismCode>
+                </CardContent>
+              </Card>
+            </div>
+            <img style={{ padding: 32 }} height={500} src={NetworkExample} />
+          </Flex>
+        </Step>
+        <Step id="data-lists" x={1100} y={3400} z={200} rotate={-5}>
+          <Flex style={{ alignItems: 'center', marginLeft: -100 }}>
+            <div style={{ width: 800 }}>
+              <Card>
+                <CardContent>
+                  <div style={{ textAlign: 'center', fontSize: 24 }}>
+                    Let's display GitHubAPI's results in a list:
+                  </div>
+                  <PrismCode component="pre" className="language-jsx">
+                    {`import { View, Text, FlatList } from 'react-native';
+
+class RepositoryList extends React.Component {
+  keyExtractor = (item) => item.id;
+  renderItem = ({ item }) =>
+    <View style={{ padding: 16 }}>
+      <Text>{ item.full_name }</Text>
+      <Text>{ item.url }</Text>
+    </View>;
+
+  render = () =>
+    <FlatList
+      data={this.props.data.items}
+      keyExtractor={this.keyExtractor}
+      renderItem={this.renderItem}
+    />
+}
+
+class GitHubAPI extends React.Component {
+  ... // as before: initial state, API request
+  render = () => <RepositoryList data={this.state.data} />;
+}`}
+                  </PrismCode>
+                </CardContent>
+              </Card>
+            </div>
+            <img style={{ padding: 32 }} height={500} src={ListExample} />
+          </Flex>
+        </Step>
+        <Step id="handling-text-input" x={0} y={1100} z={200} rotate={-5}>
+          <Flex style={{ alignItems: 'center', marginLeft: -100 }}>
+            <div style={{ width: 800 }}>
+              <Card>
+                <CardContent>
+                  <h4>Handling text input</h4>
+                  <List>
+                    <Item>
+                      This is similar to React, we just didn't cover it yet:
+                    </Item>
+                    <PrismCode component="pre" className="language-jsx">
+                      {`import { View, TextInput, Button } from 'react-native';
+
+class MyTextForm extends React.Component {
+  state = { text: '' };
+
+  onChange = (text) => this.setState({ text });
+  onSubmit = () => this.props.onSubmit(this.state.text);
+
+  render = () =>
+    <View>
+      <TextInput
+        onChangeText={this.onChange}
+        value={this.state.text}
+      />
+      <Button
+        onPress={this.onSubmit}
+        title="Submit"
+      />
+    </View>;
+}`}
+                    </PrismCode>
+                  </List>
+                </CardContent>
+              </Card>
+            </div>
+            <img style={{ padding: 32 }} height={400} src={TextExample} />
+          </Flex>
+        </Step>
+        <Step id="styling" x={100} y={2000} z={200} rotate={-5}>
+          <Card>
+            <CardContent>
+              <h4>Styling</h4>
+              <List>
+                <Item>
+                  As we have seen, inline styling works similar to React
+                </Item>
+                <Item>
+                  Recommended to use <code>StyleSheet.create()</code> for big
+                  stylesheets:
+                </Item>
+                <PrismCode component="pre" className="language-jsx">
+                  {`import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});`}
+                </PrismCode>
+                <PrismCode component="pre" className="language-jsx">
+                  {`<Text style={styles.bigblue}> ... </Text>`}
+                </PrismCode>
+                <Item>
+                  Using <code>StyleSheet.create()</code> has several advantages:
+                </Item>
+                <List style={{ marginTop: 0 }}>
+                  <Item>Styles are validated at app launch</Item>
+                  <Item>
+                    Style objects are created once, not each time{' '}
+                    <code>render()</code> runs
+                  </Item>
+                  <Item>Styles are sent from JS to native code only once</Item>
+                </List>
+              </List>
+            </CardContent>
+          </Card>
+        </Step>
+        <Step id="flexbox-layout" x={-300} y={3000} z={200} rotate={-5}>
+          <div style={{ marginLeft: -100 }}>
+            <Card style={{ width: 930 }}>
+              <CardContent>
+                <h4>Layout and flexbox</h4>
+                <List>
+                  <Item>
+                    By default, React Native components use flexbox layout
+                  </Item>
+                  <Item>
+                    Works very similar to flexbox on the web, exceptions:
+                  </Item>
+                  <List style={{ marginTop: 0 }}>
+                    <Item>
+                      Phones have portrait displays:{' '}
+                      <code>
+                        <b>flexDirection</b>
+                      </code>{' '}
+                      is set to <b>column</b> by default
+                    </Item>
+                    <Item>
+                      <code>
+                        <b>flex</b>
+                      </code>{' '}
+                      style only takes in one number:{' '}
+                      <code>
+                        <b>flexGrow</b>
+                      </code>
+                    </Item>
+                    <List style={{ marginTop: 0 }}>
+                      <Item>On the web we can pass a string to flex:</Item>
+                      <Item>
+                        <code>"flex: [flexGrow] [flexShrink] [flexBasis]"</code>
+                      </Item>
+                      <Item>
+                        Note that we can still use all of these in React Native
+                        by setting{' '}
+                        <code>
+                          <b>flexShrink</b>
+                        </code>{' '}
+                        and{' '}
+                        <code>
+                          <b>flexBasis</b>
+                        </code>
+                      </Item>
+                    </List>
+                  </List>
+                </List>
+                <Flex
+                  style={{ justifyContent: 'center', alignItems: 'flex-end' }}
+                >
+                  <PrismCode component="pre" className="language-jsx">
+                    {`import { View } from 'react-native';
+
+class FlexBoxExample extends React.Component {
+  render = () =>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, backgroundColor: '#aaf' }} />
+      <View style={{ flex: 1, flexDirection: 'row' }}>
+        <View style={{ flex: 1, backgroundColor: '#faa' }} />
+        <View style={{ flex: 1, backgroundColor: '#afa' }} />
+      </View>
+    </View>
+}`}
+                  </PrismCode>
+                  <img style={{ padding: 16 }} height={300} src={FlexExample} />
+                </Flex>
+              </CardContent>
+            </Card>
+          </div>
+        </Step>
+        <Step id="external-libraries" x={-1200} y={2000} z={200} rotate={-5}>
+          <Card>
+            <CardContent>
+              <h4>React Native libraries</h4>
+              <List>
+                <Item>Plenty of UI component libraries exist, such as: </Item>
+                <a href="https://nativebase.io/">https://nativebase.io/</a>
+              </List>
+            </CardContent>
+          </Card>
+        </Step>
+        <Step id="networking-exercise" x={-1500} y={3000} z={300} rotate={-7}>
+          <Flex
+            style={{ width: 1000, marginLeft: -100, alignItems: 'flex-end' }}
+          >
+            <div style={{ flex: 3 }}>
+              <Card>
+                <CardContent>
+                  <Handwriting>Coding exercise</Handwriting>
+                  <div style={{ fontSize: 23 }}>
+                    List the <b>names</b> and <b>authors</b> of GitHub
+                    repositories that <b>match a search string</b> given by the
+                    user.
+                  </div>
+                </CardContent>
+              </Card>
+              <Card style={{ marginTop: 16 }}>
+                <CardContent>
+                  <Small>
+                    1. Implement a{' '}
+                    <code>
+                      <b>&lt;GitHubAPI&gt;</b>
+                    </code>{' '}
+                    component which:
+                    <List style={{ marginTop: 0 }}>
+                      <Item>
+                        Calls{' '}
+                        <code>
+                          <b>fetch()</b>
+                        </code>{' '}
+                        in its{' '}
+                        <code>
+                          <b>componentDidMount()</b>
+                        </code>
+                      </Item>
+                      <Item>Stores the result in state</Item>
+                      <Item>
+                        Displays current state using a{' '}
+                        <code>
+                          <b>&lt;Text&gt;</b>
+                        </code>{' '}
+                        component.
+                      </Item>
+                    </List>
+                  </Small>
+                  <Tiny style={{ paddingTop: 8 }}>
+                    <b>Hint</b>: you need to call{' '}
+                    <code>
+                      <b>JSON.stringify(data, 0, 2)</b>
+                    </code>{' '}
+                    on the data when rendering it as text.
+                  </Tiny>
+                </CardContent>
+              </Card>
+            </div>
+            <div style={{ flex: 5, paddingLeft: 16 }}>
+              <Card>
+                <CardContent>
+                  <Small>
+                    2. Using{' '}
+                    <code>
+                      <b>&lt;TextInput&gt;</b>
+                    </code>, take in a string from the user and store it in{' '}
+                    <code>
+                      <b>&lt;GitHubAPI&gt;</b>
+                    </code>
+                    {"'"}s state.
+                  </Small>
+                </CardContent>
+              </Card>
+              <Card style={{ marginTop: 16 }}>
+                <CardContent>
+                  <Small>
+                    3. Call the API endpoint again whenever the TextInput field
+                    changes, with the query string in the URL set to:
+                    <b>"?q=&lt;TextInput field state&gt;"</b>
+                  </Small>
+                </CardContent>
+              </Card>
+              <Card style={{ marginTop: 16 }}>
+                <CardContent>
+                  <Small>
+                    4. Instead of displaying the results in a{' '}
+                    <code>
+                      <b>&lt;Text&gt;</b>
+                    </code>{' '}
+                    component, create a{' '}
+                    <code>
+                      <b>&lt;RepositoryList&gt;</b>
+                    </code>{' '}
+                    component which takes in a list of data as props and renders
+                    the data into a{' '}
+                    <code>
+                      <b>&lt;FlatList&gt;</b>
+                    </code>{' '}
+                  </Small>
+                </CardContent>
+              </Card>
+              <Card style={{ marginTop: 16 }}>
+                <CardContent>
+                  <Small>
+                    <b>Extra</b>: Also display each GitHub user's avatar in the
+                    list with{' '}
+                    <code>
+                      <b>&lt;Image&gt;</b>
+                    </code>.
+                  </Small>
+                </CardContent>
+              </Card>
+            </div>
+          </Flex>
+          <Card style={{ marginTop: 16, width: 1000, marginLeft: -100 }}>
+            <CardContent>
+              <Tiny>
+                The <b>API endpoint</b> is at:{' '}
+                <b>
+                  {'https://api.github.com/search/repositories?q=react'}
+                </b>{' '}
+                where <b>react</b> is an example query
+              </Tiny>
             </CardContent>
           </Card>
         </Step>
